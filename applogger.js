c1e8f9f6dbb798f8cac77c8d4872cfd870a1d3af
Oleg31337 <oleg31337@yahoo.com>
2021-11-19 18:16:14 -0500

    change the app logger UTC time to local time

--- applogger.js
+++ applogger.js
@@ -1,13 +1,22 @@
 //const fs = require('fs');
 const appOptions = require ('./app-options.json'); // read configuration file
-const log = function (...theArgs){ console.log((new Date()).toISOString()+' '+theArgs) }
+const log = function (...theArgs){ 
+  var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+  var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+  console.log(localISOTime+' '+theArgs)
+}
 const debug = function (...theArgs){
   if (appOptions.DEBUG == true) {
-    console.log((new Date()).toISOString()+' '+theArgs);
+    var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+    console.log(localISOTime+' '+theArgs)
   }
 }
-const error = function (...theArgs){ console.error((new Date()).toISOString()+' '+theArgs) }
-
+const error = function (...theArgs){
+  var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+  var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+  console.error(localISOTime+' '+theArgs)
+}
 module.exports.log = log;
 module.exports.debug = debug;
 module.exports.error = error;

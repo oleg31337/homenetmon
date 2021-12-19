86c6486420114db608469b6878578bd0949146c0
Oleg31337 <oleg31337@yahoo.com>
2021-12-18 21:48:31 -0500

    Fix: added space between date and error text

--- applogger.js
+++ applogger.js
@@ -13,7 +13,7 @@ const debug = function (...theArgs){
 const error = function (...theArgs){
   var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
   var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
-  console.error(localISOTime+theArgs);
+  console.error(localISOTime+' '+theArgs);
 }
 module.exports.log = log;
 module.exports.debug = debug;
